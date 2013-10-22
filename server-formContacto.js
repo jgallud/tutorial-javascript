ef5add78f8bb3a1a53381b52d6e823a92d6b5670
jgallud <jose.gallud@uclm.es>
2013-10-22 19:44:40 +0200

    ejemplo de formulario contacto en nodejs y ajax

---
+++ server-formContacto.js
@@ -0,0 +1,37 @@
+
+
+var fs=require("fs");
+var config=JSON.parse(fs.readFileSync("config.json"));
+var host=config.host;
+var port=config.port;
+var exp=require("express");
+var app=exp(); //el tutorial indicaba exp.createServer()
+
+app.use(app.router);
+app.use('/',exp.static(__dirname + "/formContacto"));
+
+/*
+app.get("/",function(request,response){
+	var contenido=fs.readFileSync("./ajax30.html");
+	response.setHeader("Content-type","text/html");
+	response.send(contenido);
+});
+*/
+
+app.post("/peticion",function(request,response){
+	var body='';
+	//console.log("petición post recibida");
+	request.on('data', function(chunk) {
+		body+=chunk.toString();      
+    });
+    
+    request.on('end', function() {
+      // empty 200 OK response for now
+      response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      response.end();
+      console.log(body);
+    });
+});
+
+console.log("servidor iniciado...");
+app.listen(port,host);
