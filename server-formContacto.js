4faaa69c42c045b9410bddd0be4e950ef6a5fb1f
jgallud <jgallud@gmail.com>
2014-10-20 12:28:02 +0200

    ejemplo formulario ajax

--- server-formContacto.js
+++ server-formContacto.js
@@ -7,12 +7,12 @@ var port=config.port;
 var exp=require("express");
 var app=exp(); //el tutorial indicaba exp.createServer()
 
-app.use(app.router);
+//app.use(app.router);
 app.use('/',exp.static(__dirname + "/formContacto"));
 
 
 app.get("/",function(request,response){
-	var contenido=fs.readFileSync("./ajax30.html");
+	var contenido=fs.readFileSync("./formContacto/ajax30.html");
 	response.setHeader("Content-type","text/html");
 	response.send(contenido);
 });
@@ -23,15 +23,15 @@ app.post("/peticion",function(request,response){
 	//console.log("petición post recibida");
 	request.on('data', function(chunk) {
 		body+=chunk;//chunk.toString();      
+    //console.log(body);
     });
     
     request.on('end', function() {
       // empty 200 OK response for now
       var resultado=JSON.parse(body);
+      console.log(resultado);
       response.writeHead(200, "OK", {'Content-Type': 'text/html'});     
       response.end();
-
-      console.log(resultado);
     });
 });
 
